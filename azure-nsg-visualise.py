1c629f19cef0d185732910d0564f261c778105b9
Michael Spinks <mikespinks@gmail.com>
2018-10-28 18:46:43 +0000

    Add just Python file

---
+++ azure-nsg-visualise.py
@@ -0,0 +1,18 @@
+
+import csv
+
+
+print("digraph NSG { \n\trankdir=LR; \n\tsize=""8.5""; \n\tnode [shape = rectangle];")
+
+print("[ label = ];")
+
+with open('C:\\Users\\Michael\\Desktop\\Graphviz\\TestNSG01.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print ("\t\"",row['SourceAddressPrefix'],"\"","->","\"",row['DestinationAddressPrefix'],"\"","[ label = ","\"",row['Access'],row['DestinationPortRange'],row['Direction'],"\"","];")
+
+print("}")
+
+
