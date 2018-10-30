2e9862239ee27c1fccafdafd61b8fac28e2ac614
mikespinks <mikespinks@gmail.com>
2018-10-30 08:46:18 +0000

    Update to Add
    Description -> SourceAddressPrefix as a independent edge.

--- azure-nsg-visualise.py
+++ azure-nsg-visualise.py
@@ -4,12 +4,13 @@ import csv
 
 print("digraph NSG { \n\trankdir=LR; \n\tnode [shape = rectangle];")
 
-with open('C:\\Users\\mikespinks\\Desktop\\NSG Visualise\\Prod\\NSG.csv', mode='r') as csv_file:
+with open('C:\\Users\\mikespinks\\Desktop\\Test\\NSG.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print ("\t\"",row['Name'],"\"", "->", "\"",row['SourceAddressPrefix'], "\"", "->", "\"",row['DestinationAddressPrefix'], "\"", "[ label = ", "\"", row['Action'], "traffic", row['Type'], " to ", row['DestinationPortRange'], " using", row['Protocol'], "Protocol", "\"""];")
+            print("\t\"", row['Name'], "\"", "->", "\"", row['SourceAddressPrefix'], "\""";")
+            print ("\t\"",row['SourceAddressPrefix'], "\"", "->", "\"",row['DestinationAddressPrefix'], "\"", "[ label = ", "\"", row['Action'], "traffic", row['Type'], " to ", row['DestinationPortRange'], " using", row['Protocol'], "Protocol", "\"""];")
 
 print("}")
 
